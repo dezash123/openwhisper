1649fbde164ff1d3d575ccdb1b4ed441743ebdd6
dezash123 <desmondmehta@gmail.com>
2025-08-17 17:57:35 -0400

    start/stop icons, write placeholder text to clipboard

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,7 +1,9 @@
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn get_text(text: String) -> String {
+    println!("Received text: {}", text);
+    text
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
@@ -9,7 +11,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_clipboard_manager::init())
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![get_text])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
